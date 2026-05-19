5190f12acaab43b5f4bb989f56b6ba85fbe72f35
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-19 18:00:40 +0300

    Reinstall alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +10,18 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.config.settings import settings
+from src.models.base import Base
+from src.models import accounts # noqa: F401
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.postgres_database_url.replace("asyncpg", "psycopg2")
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +32,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
