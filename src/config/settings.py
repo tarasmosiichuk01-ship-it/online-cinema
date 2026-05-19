c80c3b3848af5b2efd2e9440c6bc55368a1c8884
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-19 11:21:50 +0300

    Create module config and add file with settings

---
+++ src/config/settings.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_DB_PORT: int
+    POSTGRES_DB: str
+
+    SECRET_KEY_ACCESS: str
+    SECRET_KEY_REFRESH: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @property
+    def postgres_database_url(self) -> str:
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"{self.POSTGRES_HOST}:{self.POSTGRES_DB_PORT}/{self.POSTGRES_DB}")
+
+settings = Settings()
