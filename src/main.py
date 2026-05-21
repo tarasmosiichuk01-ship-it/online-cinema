7eb282424203b0ae78e8a1fe34fa368ebe253c7d
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-21 12:30:07 +0300

    Add accounts in app.include_router in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from routes import accounts_router
+
 app = FastAPI(
     title="Online Cinema",
     description="Description of project",
@@ -7,4 +9,5 @@ app = FastAPI(
 
 api_version_prefix = "/api/v1"
 
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 
