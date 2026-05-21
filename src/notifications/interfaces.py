9faafb524c811c638971454fd33335a59eb1aba2
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-21 17:23:21 +0300

    Delete unnecessary methods in src/notifications/interfaces

--- src/notifications/interfaces.py
+++ src/notifications/interfaces.py
@@ -12,13 +12,3 @@ class EmailSenderInterface(ABC):
     async def send_activation_complete_email(self, email: str, login_link: str) -> None:
 
         pass
-
-    @abstractmethod
-    async def send_password_reset_email(self, email: str, reset_link: str) -> None:
-
-        pass
-
-    @abstractmethod
-    async def send_password_reset_complete_email(self, email: str, login_link: str) -> None:
-
-        pass
