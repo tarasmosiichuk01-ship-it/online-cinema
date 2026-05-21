b0822d94b6befc8107a9bfb45733c228ae386427
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-21 17:25:04 +0300

    Delete get_settings and add get_accounts_email_notificator methods in src/config/dependencies.py

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,7 +1,16 @@
-from functools import lru_cache
+from config.settings import settings
+from notifications.emails import EmailSender
+from notifications.interfaces import EmailSenderInterface
 
-from config.settings import Settings
 
-@lru_cache
-def get_settings() -> Settings:
-    return Settings()
+def get_accounts_email_notificator() -> EmailSenderInterface:
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+    )
