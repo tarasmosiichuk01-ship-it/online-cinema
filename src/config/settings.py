afee25a398bae9cdd25805eabbe0fea3e5280806
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-21 12:28:07 +0300

    Add BASE_DIR ti model_config in src/config/settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class Settings(BaseSettings):
     POSTGRES_USER: str
@@ -11,7 +14,7 @@ class Settings(BaseSettings):
     SECRET_KEY_ACCESS: str
     SECRET_KEY_REFRESH: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=str(BASE_DIR / ".env"))
 
     @property
     def postgres_database_url(self) -> str:
