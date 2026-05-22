cbc1d0ad45d05ba92005833e0e1cecdb9b3ceb15
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-22 11:31:30 +0300

    Add activate_token endpoint to src/routes/accounts.py

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -1,19 +1,26 @@
+from datetime import datetime, timezone
+
 from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from config.dependencies import get_accounts_email_notificator
 from database import get_postgresql_db
 from notifications.interfaces import EmailSenderInterface
-from schemas.accounts import UserRegistrationRequestSchema, UserRegistrationResponseSchema
+from schemas.accounts import UserRegistrationRequestSchema, UserRegistrationResponseSchema, MessageResponseSchema
 from models.accounts import User, UserGroup, UserGroupEnum, ActivationToken
 
 
 router = APIRouter()
 
 
-@router.post("/register/", response_model=UserRegistrationResponseSchema, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=status.HTTP_201_CREATED
+)
 async def register_user(
     user_data: UserRegistrationRequestSchema,
     db: AsyncSession = Depends(get_postgresql_db),
@@ -68,3 +75,58 @@ async def register_user(
     )
 
     return UserRegistrationResponseSchema.model_validate(new_user)
+
+@router.get("/activate/{token}", response_model=MessageResponseSchema, status_code=status.HTTP_200_OK)
+async def activate_token(
+    token: str,
+    db: AsyncSession = Depends(get_postgresql_db),
+    email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator)
+):
+    query = (
+        select(ActivationToken)
+        .options(joinedload(ActivationToken.user))
+        .where(ActivationToken.token == token)
+    )
+    result = await db.execute(query)
+    token_record = result.scalars().first()
+
+    if not token_record:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid or expired activation token."
+        )
+
+    expires_at = token_record.expires_at.replace(tzinfo=timezone.utc)
+    if expires_at < datetime.now(tz=timezone.utc):
+        await db.delete(token_record)
+        await db.commit()
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid or expired activation token."
+        )
+
+    user = token_record.user
+
+    if user.is_active:
+        await db.delete(token_record)
+        await db.commit()
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User account is already active."
+        )
+
+    user.is_active = True
+    await db.delete(token_record)
+    await db.commit()
+
+
+    login_link = "http://127.0.0.1/accounts/login/"
+
+    await email_sender.send_activation_complete_email(
+        email=user.email,
+        login_link=login_link
+    )
+
+    return MessageResponseSchema(message="User account activated successfully.")
+
