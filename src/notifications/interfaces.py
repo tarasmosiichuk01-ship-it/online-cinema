bc3dc432f4acfc3af222ec6afb5f45938a76becc
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-23 13:40:53 +0300

    Add send_password_reset_email to EmailSenderInterface in src/notifications/interfaces.py

--- src/notifications/interfaces.py
+++ src/notifications/interfaces.py
@@ -5,10 +5,12 @@ class EmailSenderInterface(ABC):
 
     @abstractmethod
     async def send_activation_email(self, email: str, activation_link: str) -> None:
-
         pass
 
     @abstractmethod
     async def send_activation_complete_email(self, email: str, login_link: str) -> None:
+        pass
 
+    @abstractmethod
+    async def send_password_reset_email(self, email: str, reset_link:str) -> None:
         pass
