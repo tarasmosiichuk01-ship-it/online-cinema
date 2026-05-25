1634814af925b8aa9981eb4ac5411e45dc2f6d71
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 22:32:23 +0300

    Add movies_router to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import accounts_router
+from routes import accounts_router, movies_router
 
 app = FastAPI(
     title="Online Cinema",
@@ -10,4 +10,5 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(movies_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"])
 
