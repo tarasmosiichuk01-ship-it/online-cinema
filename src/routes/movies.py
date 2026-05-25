be72686ed331862bed491b5c69faf6124ee3ec8d
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 18:24:57 +0300

    Add get_movie_list endpoint to src/routes/movies.py

---
+++ src/routes/movies.py
@@ -0,0 +1,47 @@
+import math
+
+from fastapi import APIRouter, status, Query, Depends, HTTPException
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_postgresql_db
+from models.movies import Movie
+from schemas.movies import MovieListResponseSchema, MovieListItemSchema
+
+router = APIRouter()
+
+
+@router.get(
+    "/movies",
+    response_model=MovieListResponseSchema,
+    status_code=status.HTTP_200_OK
+)
+async def get_movie_list(
+    page: int = Query(1, ge=1, description="Page number (1-based index)"),
+    per_page: int = Query(10, ge=1, le=20, description="Number of items per page"),
+    db: AsyncSession = Depends(get_postgresql_db),
+) -> MovieListResponseSchema:
+
+    total_items = (await db.execute(select(func.count()).select_from(Movie))).scalar()
+    total_pages = 1 if total_items == 0 else math.ceil(total_items / per_page)
+    prev_page = f"/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    queryset = (
+        select(Movie).order_by(Movie.id.desc()).offset((page - 1) * per_page).limit(per_page)
+    )
+    result = await db.execute(queryset)
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movies found.")
+
+    movie_list = [MovieListItemSchema.model_validate(movie) for movie in movies]
+
+    return MovieListResponseSchema(
+        movies=movie_list,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
