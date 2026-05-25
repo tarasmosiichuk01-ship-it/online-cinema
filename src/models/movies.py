2cc26d2abfb238dfde1216c8d10a1f442f4e311d
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 11:00:43 +0300

    Add Movie model to src/models/movies.py

--- src/models/movies.py
+++ src/models/movies.py
@@ -1,4 +1,7 @@
-from sqlalchemy import String, Table, Column, ForeignKey
+from typing import Optional
+from uuid import UUID, uuid4
+
+from sqlalchemy import String, Table, Column, ForeignKey, Integer, Float, Text, DECIMAL
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models.base import Base
@@ -68,3 +71,41 @@ class Certification(Base):
     name: Mapped[str] = mapped_column(String(63), unique=True, nullable=False)
 
     movies: Mapped[list["Movie"]] = relationship("Movie", back_populates="certification")
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    uuid: Mapped[UUID] = mapped_column(default=uuid4)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    time: Mapped[int] = mapped_column(Integer, nullable=False)
+    imdb: Mapped[float] = mapped_column(Float, nullable=False)
+    votes: Mapped[int] = mapped_column(Integer, nullable=False)
+    meta_score: Mapped[Optional[float]] = mapped_column(Float, nullable=False)
+    gross: Mapped[Optional[float]] = mapped_column(Float, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    price: Mapped[float] = mapped_column(DECIMAL(10, 2), nullable=False)
+
+    certification_id: Mapped[int] = mapped_column(ForeignKey("certifications.id"), nullable=False)
+    certification: Mapped["Certification"] = relationship("Certification", back_populates="movies")
+
+    genres: Mapped[list["Genre"]] = relationship(
+        "Genre",
+        secondary=MoviesGenres,
+        back_populates="movies"
+    )
+
+    directors: Mapped[list["Director"]] = relationship(
+        "Director",
+        secondary=MoviesDirectors,
+        back_populates="movies"
+    )
+
+    stars: Mapped[list["Star"]] = relationship(
+        "Star",
+        secondary=MoviesStars,
+        back_populates="movies"
+    )
+
