fe9086325454aac022dfd96fbac68223b318ed74
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 10:28:54 +0300

    Add Genre model to src/models/movies.py

---
+++ src/models/movies.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String, Table, Column, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+
