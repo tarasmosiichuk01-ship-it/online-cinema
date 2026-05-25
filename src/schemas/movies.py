6b83e19e0ad59e340bb1a4b5d3153b6b64b4ed6d
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 17:38:07 +0300

    Add MovieBaseSchema & MovieDetailSchema to src/schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,4 +1,9 @@
-from pydantic import BaseModel
+import decimal
+from datetime import datetime
+from typing import List
+from uuid import UUID
+
+from pydantic import BaseModel, Field, field_validator
 
 
 class GenreSchema(BaseModel):
@@ -25,5 +30,38 @@ class DirectorSchema(BaseModel):
 class CertificationSchema(BaseModel):
     id: int
     name: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieBaseSchema(BaseModel):
+    name: str = Field(..., max_length=100)
+    year: int
+    time: int = Field(..., ge=0)
+    imdb: float = Field(..., ge=0)
+    votes: int = Field(..., ge=0)
+    meta_score: float = Field(..., ge=0, le=10)
+    gross: float = Field(..., ge=0)
+    description: str
+    price: decimal.Decimal = Field(..., ge=0)
+
+    model_config = {"from_attributes": True}
+
+    @field_validator("year")
+    @classmethod
+    def validate_year(cls, value):
+        current_year = datetime.now().year
+        if value.year > current_year + 1:
+            raise ValueError(f"The year in 'year' cannot be greater than {current_year + 1}.")
+        return value
+
+
+class MovieDetailSchema(MovieBaseSchema):
+    id: int
+    uuid: UUID
+    certification: CertificationSchema
+    genres: List[GenreSchema]
+    stars: List[StarSchema]
+    directors: List[DirectorSchema]
 
     model_config = {"from_attributes": True}
