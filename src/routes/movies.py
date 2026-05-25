4e257fb3b190aa7b66c5adf0eae953d4f269e443
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-25 19:16:43 +0300

    Add get_genre_list endpoint to src/routes/movies.py

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -6,16 +6,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from database import get_postgresql_db
-from models.movies import Movie
-from schemas.movies import MovieListResponseSchema, MovieListItemSchema, MovieDetailSchema
+from models.movies import Movie, Genre
+from schemas.movies import MovieListResponseSchema, MovieListItemSchema, MovieDetailSchema, \
+    GenreListResponseSchema, GenreListItemSchema
 
 router = APIRouter()
 
 
 @router.get(
     "/movies",
-    response_model=MovieListResponseSchema,
-    status_code=status.HTTP_200_OK
+    response_model=MovieListResponseSchema
 )
 async def get_movie_list(
     page: int = Query(1, ge=1, description="Page number (1-based index)"),
@@ -74,3 +74,18 @@ async def get_movie_by_id(movie_id: int, db: AsyncSession = Depends(get_postgres
         )
 
     return MovieDetailSchema.model_validate(movie)
+
+
+@router.get("/genres", response_model=GenreListResponseSchema)
+async def get_genre_list(db: AsyncSession = Depends(get_postgresql_db)) -> GenreListResponseSchema:
+
+    query = select(Genre).order_by(Genre.id.desc())
+    result = await db.execute(query)
+    genres = result.scalars().all()
+
+    if not genres:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No genres found.")
+
+    genre_list = [GenreListItemSchema.model_validate(genre) for genre in genres]
+
+    return GenreListResponseSchema(genres=genre_list)
