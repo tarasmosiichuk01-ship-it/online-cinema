9015628b0e518b935161a90bd49a61296908b4cc
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-29 13:14:09 +0300

    Add reply_comment_template_name to get_accounts_email_notificator in src/config/dependencies.py

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -93,6 +93,7 @@ def get_accounts_email_notificator() -> EmailSenderInterface:
         activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
         activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        reply_comment_template_name=settings.REPLY_COMMENT_TEMPLATE_NAME,
     )
 
 
