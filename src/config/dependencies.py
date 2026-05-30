2e4da3115d02356604595abbaabb03a0f29a405a
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-30 11:57:16 +0300

    Add reaction_comment_template_name to get_accounts_email_notificator in dependencies.py

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -94,6 +94,7 @@ def get_accounts_email_notificator() -> EmailSenderInterface:
         activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
         reply_comment_template_name=settings.REPLY_COMMENT_TEMPLATE_NAME,
+        reaction_comment_template_name=settings.REACTION_COMMENT_TEMPLATE_NAME
     )
 
 
