0972abdea95bfd4e9b770bc5da1fbd89a0b957c6
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-02 14:45:03 +0300

    Add GenreShortResponseSchema to src/schemas/shopping_carts.py

--- src/schemas/shopping_carts.py
+++ src/schemas/shopping_carts.py
@@ -6,3 +6,11 @@ from pydantic import BaseModel, ConfigDict
 
 class CartItemCreateSchema(BaseModel):
     movie_id: int
+
+
+class GenreShortResponseSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
