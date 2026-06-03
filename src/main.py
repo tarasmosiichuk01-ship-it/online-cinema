3e743d330c8c8f8ce1a263bf4d898f9f3f815432
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-03 21:35:32 +0300

    Add orders_router route to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import accounts_router, shopping_carts_router
+from routes import accounts_router, shopping_carts_router, orders_router
 from routes.cinema import cinema_router
 
 app = FastAPI(
@@ -13,3 +13,4 @@ api_version_prefix = "/api/v1"
 app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 app.include_router(cinema_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"])
 app.include_router(shopping_carts_router, prefix=f"{api_version_prefix}/shopping_carts", tags=["shopping_carts"])
+app.include_router(orders_router, prefix=f"{api_version_prefix}/orders", tags=["orders"])
