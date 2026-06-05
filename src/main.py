810c189d19b51c0350c093f3fca3d4c4188b0bc9
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-05 22:21:09 +0300

    Add payment route to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import accounts_router, shopping_carts_router, orders_router
+from routes import accounts_router, shopping_carts_router, orders_router, payments_router
 from routes.cinema import cinema_router
 
 app = FastAPI(
@@ -14,3 +14,4 @@ app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tag
 app.include_router(cinema_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"])
 app.include_router(shopping_carts_router, prefix=f"{api_version_prefix}/shopping_carts", tags=["shopping_carts"])
 app.include_router(orders_router, prefix=f"{api_version_prefix}/orders", tags=["orders"])
+app.include_router(payments_router, prefix=f"{api_version_prefix}/payments", tags=["payments"])
