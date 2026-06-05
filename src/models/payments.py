5878da5a3ee77d9ba37179ccff15bd6e41b84192
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-05 12:56:19 +0300

    Add Payment model to src/models/payments.py

--- src/models/payments.py
+++ src/models/payments.py
@@ -1,4 +1,10 @@
+import decimal
 import enum
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlalchemy import ForeignKey, DateTime, func, Enum, DECIMAL
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
 
@@ -6,3 +12,40 @@ class PaymentStatusEnum(str, enum.Enum):
     SUCCESSFUL = "successful"
     CANCELED = "canceled"
     REFUNDED = "refunded"
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="payments")
+
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), nullable=False)
+    order: Mapped["Order"] = relationship("Order", back_populates="payments")
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
+
+    status: Mapped[PaymentStatusEnum] = mapped_column(
+        Enum(PaymentStatusEnum),
+        nullable=False,
+        default=PaymentStatusEnum.SUCCESSFUL
+    )
+
+    amount: Mapped[decimal.Decimal] = mapped_column(
+        DECIMAL(10, 2),
+        nullable=False
+    )
+
+    external_payment_id: Mapped[Optional[str]] = mapped_column(nullable=True)
+
+    payment_items: Mapped[list["PaymentItem"]] = relationship("PaymentItem", back_populates="payment")
+
+
+
