ff10bba017590d627c052b67022107218aa35950
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-05 22:20:13 +0300

    Add confirmation_payment_template_name to get_accounts_email_notificator in src/config/dependencies.py

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -96,7 +96,8 @@ def get_accounts_email_notificator() -> EmailSenderInterface:
         activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
         reply_comment_template_name=settings.REPLY_COMMENT_TEMPLATE_NAME,
-        reaction_comment_template_name=settings.REACTION_COMMENT_TEMPLATE_NAME
+        reaction_comment_template_name=settings.REACTION_COMMENT_TEMPLATE_NAME,
+        confirmation_payment_template_name=settings.CONFIRMATION_PAYMENT_TEMPLATE_NAME,
     )
 
 
