8a4a8e0e071c700660b854d4877c86e58867ef0f
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-05 22:19:00 +0300

    Add send_confirmation_payment_email method to EmailSender in  src/notifications/emails.py

--- src/notifications/emails.py
+++ src/notifications/emails.py
@@ -23,7 +23,8 @@ class EmailSender(EmailSenderInterface):
         activation_complete_email_template_name: str,
         password_email_template_name: str,
         reply_comment_template_name: str,
-        reaction_comment_template_name: str
+        reaction_comment_template_name: str,
+        confirmation_payment_template_name: str
     ):
         self._hostname = hostname
         self._port = port
@@ -35,6 +36,7 @@ class EmailSender(EmailSenderInterface):
         self._password_email_template_name = password_email_template_name
         self._reply_comment_template_name = reply_comment_template_name
         self._reaction_comment_template_name = reaction_comment_template_name
+        self._confirmation_payment_template_name = confirmation_payment_template_name
 
         self._env = Environment(loader=FileSystemLoader(template_dir))
 
@@ -89,4 +91,10 @@ class EmailSender(EmailSenderInterface):
         template = self._env.get_template(self._reaction_comment_template_name)
         html_content = template.render(email=email, comment_link=comment_link)
         subject = "Reaction Comment"
+        await self._send_email(email, subject, html_content)
+
+    async def send_confirmation_payment_email(self, email: str, order_link: str) -> None:
+        template = self._env.get_template(self._confirmation_payment_template_name)
+        html_content = template.render(email=email, order_link=order_link)
+        subject = "Confirmation Payment"
         await self._send_email(email, subject, html_content)
