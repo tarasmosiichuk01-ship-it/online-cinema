bc31c5448813846bcd658ca1e2d17949b950094e
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-05 22:20:44 +0300

    Add import payments_router in src/routes/init.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,3 +1,4 @@
 from routes.accounts import router as accounts_router
 from routes.shopping_carts import router as shopping_carts_router
 from routes.orders import router as orders_router
+from routes.payments import router as payments_router
