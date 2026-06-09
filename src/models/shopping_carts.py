66ab5c566ebafb0ddf9c6d9ab81e7d3ca48ebe57
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-09 15:52:43 +0300

    Add PurchasedMovie model to src/models/shopping_carts.py

--- src/models/shopping_carts.py
+++ src/models/shopping_carts.py
@@ -42,3 +42,26 @@ class CartItem(Base):
     __table_args__ = (
         UniqueConstraint("cart_id", "movie_id", name="unique_cart_item_constraint"),
     )
+
+
+class PurchasedMovie(Base):
+    __tablename__ = "user_purchased_movies"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="purchased_movies")
+
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
+    movie: Mapped["Movie"] = relationship("Movie", back_populates="purchased_movies")
+
+    purchased_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "movie_id", name="unique_user_movie_purchase"),
+    )
