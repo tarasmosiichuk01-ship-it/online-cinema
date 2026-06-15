f273e882aa5e813be711ecd80650fe09ed053b73
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-15 11:24:33 +0300

    Add test_rate_movie_if_movie_not_found to src/tests/test_integration/test_interactions.py

--- src/tests/test_integration/test_interactions.py
+++ src/tests/test_integration/test_interactions.py
@@ -503,3 +503,20 @@ async def test_rate_movie_unauthorized_user(client, test_movie):
     assert response.status_code == 401
     assert response.json()["detail"] == "Not authenticated"
 
+
+@pytest.mark.asyncio
+async def test_rate_movie_if_movie_not_found(authorized_client):
+    """
+    Test rating a movie that does not exist.
+
+    Ensures that the endpoint returns a 404 status code and an appropriate
+    error message when the movie with the given ID does not exist.
+    """
+    client, user = authorized_client
+
+    payload = {"rating": 10}
+
+    response = await client.post(f"/api/v1/cinema/movies/999999999/rate", json=payload)
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Movie with the given ID was not found."
+
