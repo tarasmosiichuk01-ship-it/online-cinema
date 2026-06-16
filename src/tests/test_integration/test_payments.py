64a25e0c27401dca8adec2f20efd7633754e956a
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-16 16:46:48 +0300

    Add test_create_checkout_session_if_order_status_not_pending to src/tests/test_integration/test_payments.py

--- src/tests/test_integration/test_payments.py
+++ src/tests/test_integration/test_payments.py
@@ -40,3 +40,35 @@ async def test_create_checkout_session_if_order_not_found(authorized_client):
 
     assert response.status_code == 404
     assert response.json()["detail"] == "Order not found"
+
+
+@pytest.mark.asyncio
+async def test_create_checkout_session_if_order_status_not_pending(authorized_client, test_movie, db_session_commit):
+    """
+    Test creating a checkout session when the order is not in PENDING status.
+
+    Ensures that the endpoint returns a 400 status code and an appropriate
+    error message when the user attempts to pay for an order that is
+    not in PENDING status.
+    """
+    client, user = authorized_client
+
+    order = Order(
+        user_id=user.id,
+        status=OrderStatusEnum.PAID,
+        total_amount=test_movie.price,
+    )
+    db_session_commit.add(order)
+    await db_session_commit.commit()
+
+    payload = {
+        "order_id": order.id
+    }
+
+    response = await client.post("/api/v1/payments/payments/create-checkout-session", json=payload)
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "You can only pay for pending orders"
+
+    await db_session_commit.delete(order)
+    await db_session_commit.commit()
