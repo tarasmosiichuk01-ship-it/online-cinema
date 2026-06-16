606d65896c1a15d80a04913cf40463f8d077b59c
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-16 11:37:13 +0300

    Add test_create_order_if_carts_is_empty to src/tests/test_integration/test_orders.py

--- src/tests/test_integration/test_orders.py
+++ src/tests/test_integration/test_orders.py
@@ -12,6 +12,21 @@ async def test_create_order_unauthorized_user(client):
     response = await client.post("/api/v1/orders/orders")
 
     assert response.status_code == 401
-
     assert response.json()["detail"] == "Not authenticated"
+
+
+@pytest.mark.asyncio
+async def test_create_order_if_carts_is_empty(authorized_client):
+    """
+    Test creating an order when the cart is empty.
+
+    Ensures that the endpoint returns a 400 status code and an appropriate
+    error message when the user attempts to create an order with an empty cart.
+    """
+    client, user = authorized_client
+
+    response = await client.post("/api/v1/orders/orders")
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Your cart is empty"
 
