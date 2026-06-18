da9bee49627293491b503158a2a30a87164306fd
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-06-18 11:52:50 +0300

    Add docstring to src/config/database.py

--- src/config/database.py
+++ src/config/database.py
@@ -19,6 +19,13 @@ AsyncPostgresqlSession = async_sessionmaker(
 
 
 async def get_postgresql_db() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provide an asynchronous database session.
 
+    This function returns an async generator yielding a new database session.
+    It ensures that the session is properly closed after use.
+
+    :return: An asynchronous generator yielding an AsyncSession instance.
+    """
     async with AsyncPostgresqlSession() as session:
         yield session
